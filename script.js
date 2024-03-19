0bba2b81fdd9f792d8314f0a54d28a89000da53d
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-03-19 15:21:26 +0200

    created javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnEl= document.getElementById("btn")
+const apiKey = "gQ+zx0DqBq4Z73iNUYQJYQ==Ovxd8MIf5f13ogJE"
+const options= {
+    method : "GET",
+    headers : {
+        "X-Api-Key" : apiKey
+    },
+};
+
+
+function getjoke(){
+   
+}
+ 
+btnEl.addEventListener("click", getjoke)
