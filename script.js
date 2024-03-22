961583de00cd4e549ba281fbac387d2fa13f124f
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-03-22 10:28:46 +0200

    updated script

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const btnEl= document.getElementById("btn")
+const btnEl= document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
 const apiKey = "gQ+zx0DqBq4Z73iNUYQJYQ==Ovxd8MIf5f13ogJE"
 const options= {
     method : "GET",
     headers : {
-        "X-Api-Key" : apiKey
+        "X-Api-Key" : apiKey,
     },
 };
+const apiURL= "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
+async function getjoke(){
+    jokeEl.innerHTML = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerHTML = "Loadind...";
+   const response = await fetch(apiURL, options);
+   const data = await response.json();
 
-function getjoke(){
-   
+   btnEl.disabled = false;
+    btnEl.innerHTML = "Tell me a joke";
+   jokeEl.innerHTML = data[0].joke;
 }
  
 btnEl.addEventListener("click", getjoke)
